f61489d590691dd8c56b10e330901166edb7a26f
Oladapo Olusola <Oladapo.O@bullseye.local>
2023-05-06 04:27:06 +0100

    new commit

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 function darkMode(){
     let modee = document.querySelector('.mode');
     
-     if(modee.innerHTML === `SWITCH TO DARK MODE`){
+     if(modee.innerHTML === `DARK MODE`){
 
-        modee.innerHTML = `SWITCH TO LIGHT MODE`;
+        modee.innerHTML = `LIGHT MODE`;
         document.querySelector('body').style.backgroundColor = 'black';
         modee.classList.add('mode2')
         
      } else{
         document.querySelector('body').style.backgroundColor = 'white'
-        modee.innerHTML = `SWITCH TO DARK MODE`;
+        modee.innerHTML = `DARK MODE`;
         modee.classList.remove('mode2')
     }
       
